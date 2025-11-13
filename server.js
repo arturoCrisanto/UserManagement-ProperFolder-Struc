bf5614bd860ed0493b8a0dc81e6d9d22c27d5681
ruzell <arturocrisanto1@gmail.com>
2025-11-13 19:26:16 +0800

    fix: ensure dotenv configuration is loaded before middleware imports

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import { logger } from "./utils/logger.js";
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
